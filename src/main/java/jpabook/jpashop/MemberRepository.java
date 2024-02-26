a722e028552825bab2ed07efb1aacc3fc2f44d6b
wlsgur073 <wlsgur073@gmail.com>
2024-02-26 21:29:25 +0900

    프로젝트 기본 환경설정 및 JPA와 DB 설정, 동작 확인

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // Spring boot에서 EntityManager을 알아서 생성자 주입해준다.
+
+    public Long save(Member member) {
+        em.persist(member);
+        // 사이드 이펙트를 일으키는 커멘드 유형이기에 리턴값을 거의 만들지 않는다.
+        // 그러나 아이디 정도 반환하면 다시 조회가 가능하기에 return 해본다.
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        /*
+        * 같은 트랜잭션 안에서 meber를 저장하고 조회하면 영속성 컨텍스트가 똑같다
+        * 같은 영속성 컨텍스트 안에서 id 값이 같으면 같은 엔티티로 식별한다.
+        * 영속성 컨텍스트에서 식별자가 같으면 같은 엔티티로 인식한다고 보면 된다.
+        * */
+        assertThat(findMember).isEqualTo(member);
+    }
+}
